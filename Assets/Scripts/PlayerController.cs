daeab1e9793a97a59c2d5fe7979d6ca2d2e8d463
clubaaron <emailclubaaron@gmail.com>
2020-04-26 15:39:21 -0400

    Adds files..

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D theRB;
+
+    public float movementSpeed;
+    public bool canMove = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+        
+    // Handles player movement.
+    void Move()
+    {
+        if (canMove)
+        {
+            float deltaX = Input.GetAxisRaw("Horizontal");
+            float deltaY = Input.GetAxisRaw("Vertical");
+            theRB.velocity = new Vector2(deltaX, deltaY) * movementSpeed;
+        }
+        else
+        {
+            theRB.velocity = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    private Transform player;
+    private Transform cameraTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().transform;
+        cameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        cameraTransform.position = new Vector3(player.position.x, player.position.y, cameraTransform.position.z);
+    }
+}

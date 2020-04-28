72081b3823d4111d25e1f9f2a7fbf2be9a3145b4
clubaaron <emailclubaaron@gmail.com>
2020-04-28 16:26:37 -0400

    Adds walk animation cycles. Adds camera follow and limits movement to map and stops camera and map edges.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D theRB;
+    private Animator playerAnim;
 
     public float movementSpeed;
     public bool canMove = true;
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         theRB = gameObject.GetComponent<Rigidbody2D>();
+        playerAnim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,18 +23,46 @@ public class PlayerController : MonoBehaviour
         Move();
     }
         
-    // Handles player movement.
     void Move()
     {
         if (canMove)
         {
+            // Move player by updating velocity.
             float deltaX = Input.GetAxisRaw("Horizontal");
             float deltaY = Input.GetAxisRaw("Vertical");
             theRB.velocity = new Vector2(deltaX, deltaY) * movementSpeed;
+
+            // Set values in Animator to determine sprites to show.
+            AnimateWalk();
+            SetDirection();
         }
         else
         {
             theRB.velocity = Vector2.zero;
+        }
+
+        // Make sure player stays within map bounds.
+        float maxX = Mathf.Clamp(transform.position.x, SceneManager.instance.bottomLeftCoords.x, SceneManager.instance.topRightCoords.x);
+        float maxY = Mathf.Clamp(transform.position.y, SceneManager.instance.bottomLeftCoords.y, SceneManager.instance.topRightCoords.y);
+        float maxZ = transform.position.z;
+        transform.position = new Vector3(maxX, maxY, maxZ);
+    }
+
+    void AnimateWalk()
+    {
+        playerAnim.SetFloat("moveX", theRB.velocity.x);
+        playerAnim.SetFloat("moveY", theRB.velocity.y);
+    }
+
+    void SetDirection()
+    {
+        float horizMovement = Input.GetAxisRaw("Horizontal");
+        float vertMovement = Input.GetAxisRaw("Vertical");
+
+        if (horizMovement == 1 || horizMovement == -1 || vertMovement == 1 || vertMovement == -1)
+        {
+            playerAnim.SetFloat("lastMoveX", horizMovement);
+            playerAnim.SetFloat("lastMoveY", vertMovement);
         }
     }
 }
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SceneManager : MonoBehaviour
 {
+    public static SceneManager instance;
+
+    public Tilemap theMap;
+    public Vector3 bottomLeftCoords;
+    public Vector3 topRightCoords;
+
     private Transform player;
     private Transform cameraTransform;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
 
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
+
         player = FindObjectOfType<PlayerController>().transform;
         cameraTransform = Camera.main.transform;
+
+        SetMapBoundaries();
     }
 
     // Update is called once per frame
@@ -22,6 +35,26 @@ public class SceneManager : MonoBehaviour
 
     private void LateUpdate()
     {
+        // Camera to follow player.
         cameraTransform.position = new Vector3(player.position.x, player.position.y, cameraTransform.position.z);
+        
+        // Keep camera inside the bounds of the map.
+        float maxX = Mathf.Clamp(cameraTransform.position.x, bottomLeftLimit.x, topRightLimit.x);
+        float maxY = Mathf.Clamp(cameraTransform.position.y, bottomLeftLimit.y, topRightLimit.y);
+        float maxZ = cameraTransform.position.z;
+        cameraTransform.position = new Vector3(maxX, maxY, maxZ);
+    }
+
+    private void SetMapBoundaries() {
+        bottomLeftCoords = theMap.localBounds.min;
+        topRightCoords = theMap.localBounds.max;
+
+        float halfHeight = Camera.main.orthographicSize;
+        float halfWidth = halfHeight * Camera.main.aspect;
+        bottomLeftLimit = bottomLeftCoords + new Vector3(halfWidth, halfHeight, 0f);
+        topRightLimit = topRightCoords + new Vector3(-halfWidth, -halfHeight, 0f);
+
+        Debug.Log(bottomLeftCoords);
+        Debug.Log(bottomLeftLimit);
     }
 }
